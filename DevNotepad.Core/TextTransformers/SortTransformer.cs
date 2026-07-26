f9da0a512159fd6e56c311f57a640e7ac350c27d
Andrey Chistyakov <achistyakov@iikosoftware.com>
2026-07-26 16:28:15 +0300

    Sort Descending

--- DevNotepad.Core/TextTransformers/SortTransformer.cs
+++ DevNotepad.Core/TextTransformers/SortTransformer.cs
@@ -5,9 +5,16 @@ namespace DevNotepad.Core.TextTransformers
     [TextTransformer("Sort lines", "dc77ffa8-688d-45f3-89bd-cb94ba541a2e")]
     public class SortTransformer : BaseTextTransformer
     {
+        [TextTransformer("Sort lines descending", "ad1498c9-fbb3-4ad5-938e-edc7e84d1dd7")]
+        private static ITextTransformer BuildSortDescending() => new SortTransformer { Descending = true };
+
+        public bool Descending { get; set; }
+
         public override string[] Transform(string[] lines)
         {
-            return lines.OrderBy(s => s).ToArray();
+            return Descending
+                ? lines.OrderByDescending(s => s).ToArray()
+                : lines.OrderBy(s => s).ToArray();
         }
     }
 }
